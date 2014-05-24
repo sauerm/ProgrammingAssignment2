98a84e992d617e370a42b0f83e633a013f6c38de
Matthias Sauer <sauerm@informatik.uni-freiburg.de>
2014-05-24 13:05:08 +0200

    Implementation of Coursera R Course Programming Assignemnt 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions makeCacheMatrix and cacheSolve to store the cached result
+## of the matrix operations. 
 
-## Write a short comment describing this function
+## Example Usage:
+##    matrix =rbind(c(1, -1/4), c(-1/4, 1));
+##    cache <- makeCacheMatrix()
+##    cache$set(matrix)
+##    cacheSolve(cache)
 
+
+## makeCacheMatrix creates the cached objects to hide the cache functionallity
+## from the users. The get/set sub-functions return/store the internal
+## matrix objects. get/setInv returns/stores the inverted matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedInv <- NULL
+  set <- function(y) {
+    x <<- y
+    cachedInv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inv) cachedInv <<- inv
+  getInv <- function() cachedInv
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve retruns the inverse of x by cheching for cached data and if nothing is cached it 
+## caches the result and returns the invers by calling solve(x)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invM <- x$getInv()
+  if(!is.null(invM)) {
+    message("getting cached data")
+    return(invM)
+  }
+
+  invM <- solve(x$get())
+  x$setInv(invM)
+  return(invM)
 }
